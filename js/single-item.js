05992fa36a151d3c86b82d43663fed2c9581b9b7
guragaidipesh <guragaindipesh89@gmail.com>
2026-02-21 13:56:29 +0545

    line through issue fixes

--- js/single-item.js
+++ js/single-item.js
@@ -2,24 +2,16 @@ function createSingleItem(item) {
   var $div = $('<div class="single-item"></div>');
 
   $div.html(`
-      <input type="checkbox" ${item.completed ? "checked" : ""} />
-      <p style="text-decoration: ${item.completed ? "line-through" : "none"}">
-        ${item.name}
-      </p>
-      <button class="btn icon-btn edit-btn" type="button">
-        <i class="fa-regular fa-pen-to-square"></i>
-      </button>
-      <button class="btn icon-btn remove-btn" type="button">
-        <i class="fa-regular fa-trash-can"></i>
-      </button>
-    `);
+        <input type="checkbox" ${item.completed ? "checked" : ""} />
+       <p class="${item.completed ? "completed" : ""}">${item.name}</p>
+        <button class="btn icon-btn edit-btn" type="button">
+          <i class="fa-regular fa-pen-to-square"></i>
+        </button>
+        <button class="btn icon-btn remove-btn" type="button">
+          <i class="fa-regular fa-trash-can"></i>
+        </button>
+      `);
 
-  return $div;
-}
-function createSingleItem(item) {
-  // ....
-
-  // Add event listener for checkbox
   $div.find('input[type="checkbox"]').on("change", function () {
     editCompleted(item.id);
   });
