f2ae6eb0c12d12c6baff408cb054b89765c8a243
guragaidipesh <guragaindipesh89@gmail.com>
2026-02-21 15:38:16 +0545

    updated

--- js/app.js
+++ js/app.js
@@ -3,76 +3,14 @@ function getLocalStorage() {
   if (list) {
     return JSON.parse(list);
   }
-  return [];
+  return groceryItems;
 }
+
 function setLocalStorage(itemsArray) {
   localStorage.setItem("grocery-list", JSON.stringify(itemsArray));
 }
+
 var items = getLocalStorage();
-
-function render() {
-  var $app = $("#app");
-  $app.empty();
-
-  var $itemsElement = createItems(items);
-  $app.append($itemsElement);
-}
-
-function editCompleted(itemId) {
-  items = $.map(items, function (item) {
-    if (item.id === itemId) {
-      return $.extend({}, item, { completed: !item.completed });
-    }
-    return item;
-  });
-  setLocalStorage(items);
-  render();
-}
-$(document).ready(function () {
-  render();
-});
-
-function removeItem(itemId) {
-  items = $.grep(items, function (item) {
-    return item.id !== itemId;
-  });
-  setLocalStorage(items);
-
-  render();
-  setTimeout(function () {
-    alert("Item Deleted Successfully!");
-  }, 0);
-}
-
-function render() {
-  var $app = $("#app");
-  $app.empty();
-
-  var $formElement = createForm();
-  var $itemsElement = createItems(items);
-
-  $app.append($formElement);
-  $app.append($itemsElement);
-}
-
-function generateId() {
-  return Date.now().toString(36) + Math.random().toString(36).substr(2);
-}
-
-function addItem(itemName) {
-  var newItem = {
-    name: itemName,
-    completed: false,
-    id: generateId(),
-  };
-  items.push(newItem);
-  setLocalStorage(items);
-  render();
-  setTimeout(function () {
-    alert("Item Added Successfully!");
-  }, 0);
-}
-
 var editId = null;
 
 function render() {
@@ -84,16 +22,54 @@ function render() {
         return item.id === editId;
       })[0]
     : null;
-  var $formElement = createForm(editId, itemToEdit); // edited line
+
+  var $formElement = createForm(editId, itemToEdit);
   var $itemsElement = createItems(items);
 
   $app.append($formElement);
   $app.append($itemsElement);
 }
 
-$(document).ready(function () {
+function addItem(itemName) {
+  var newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items.push(newItem);
+
+  setLocalStorage(items);
   render();
-});
+
+  setTimeout(function () {
+    alert("Item Added Successfully!");
+  }, 0);
+}
+
+function removeItem(itemId) {
+  items = $.grep(items, function (item) {
+    return item.id !== itemId;
+  });
+
+  setLocalStorage(items);
+  render();
+
+  setTimeout(function () {
+    alert("Item Deleted Successfully!");
+  }, 0);
+}
+
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+
+  setLocalStorage(items);
+  render();
+}
 
 function updateItemName(newName) {
   items = $.map(items, function (item) {
@@ -102,19 +78,28 @@ function updateItemName(newName) {
     }
     return item;
   });
+
   editId = null;
   setLocalStorage(items);
   render();
+
   setTimeout(function () {
     alert("Item Updated Successfully!");
   }, 0);
 }
 
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
 function setEditId(itemId) {
   editId = itemId;
   render();
-
   setTimeout(function () {
     $(".form-input").focus();
   }, 0);
 }
+
+$(document).ready(function () {
+  render();
+});
